b420d06dffc7ff526ab3f5182167c2b0eb0bc75f
novak254 <ndemokelvinonkundi@gmail.com>
2020-04-22 08:12:29 +0300

    deply to heroku'

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import  SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__, template_folder='../templates', static_folder="../static")
+
+app.config['SECRET_KEY'] = 'bfdf0a9609c530f2923145e8b50ad289'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+db = SQLAlchemy(app)
+
+
+from routes import routes
